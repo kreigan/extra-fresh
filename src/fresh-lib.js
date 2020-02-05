7cabfed14c4c42744969d71d98a26406c2dc9d60
Alexander Noskov <alex.brownies@hotmail.com>
2020-02-06 06:58:14 +0800

    Committing intermittent results

--- src/fresh-lib.js
+++ src/fresh-lib.js
@@ -1,10 +1,18 @@
+_checkTagValidity = tag => {
+    const isValidTag = /^[a-z]{2,}-\d{1,10}$/i.test(tag.innerText) != false;
+    // No need to convert already processed tags
+    const isTranslated = tag.parentNode.tagName == 'A';
+    return isValidTag && !isTranslated;
+}
+
+// A base class for all screens, is not supposed to be instantiated directly
 class ScreenType {
     constructor(root) {
         this.root = root;
     }
 }
 
-// Defines a base class for filter views.
+// A base class for all filter views
 class FilterView extends ScreenType {
     constructor(root) {
         super(root);
@@ -12,8 +20,8 @@ class FilterView extends ScreenType {
         this.tagElement = 'span.list-item';
     }
 
-    // Fetches tickets and returns an array of their tags.
-    getTags() {
+    // Enumerate tickets and return an array of their tags
+    getTags = () => {
         let tickets = Array.from(this.root.querySelector(this.ticketsBlock).children);
         let tags = [];
         for (let i = 0; i < tickets.length; i++) {
@@ -24,6 +32,7 @@ class FilterView extends ScreenType {
     }
 }
 
+// Class for a card layout screen of a filter view
 class CardView extends FilterView {
     constructor(root) {
         super(root);
@@ -31,19 +40,18 @@ class CardView extends FilterView {
     }
 }
 
-// Used for table layout of a filter view.
-// Redefines the getTags() method to call only only if Tags column is shown.
+// Class for a table layout screen of a filter view
 class TableView extends FilterView {
     constructor(root) {
         super(root);
         this.tagElement = "div.show-more-list-wrapper " + this.tagElement;
     };
-    isTagsColumnShown() {
-        return this.root.querySelector('thead.lt-head') != undefined;
-    };
-    getTags() {
-        return this.isTagsColumnShown() ? super.getTags() : [];
-    }
+
+    // Check if tags are displayed at all
+    _isTagsColumnShown = () => this.root.querySelector('thead.lt-head') != undefined;
+
+    // Redefines getTags() to call it only if tags are shown
+    getTags = () => this._isTagsColumnShown() ? super.getTags() : [];
 }
 
 class TicketView extends ScreenType {
@@ -52,43 +60,37 @@ class TicketView extends ScreenType {
         this.tagElement = 'div.ticket-tags span.tag-options';
     }
 
-    getTags() {
+    getTags = () => {
         let tags = [];
         let ticketTags = this.root.querySelectorAll(this.tagElement);
-        tags.push(Array.from(ticketTags));
+        tags.push(Array.from(ticketTags.filter(_checkTagValidity)));
         return tags;
     }
 }
 
-function transformTicketTags(ticketTags) {
-    // Choose only tags where text follows the pattern.
-    let jiraTags = ticketTags.filter(
-        (tag) => {
-            const isValidTag = /^[a-z]{2,}-\d{1,10}$/i.test(tag.innerText) != false;
-            const isTranslated = tag.parentNode.tagName == 'A';
-            return isValidTag && !isTranslated;
-        });
-    if (jiraTags.length == 0) {
-        return;
-    }
-    console.debug("Ticket tags to proces:", jiraTags.length);
-    // Wrap the tag with a link element.
-    jiraTags.forEach(function (tag) {
-        let link = jiraLinkFromText(tag.innerText);
-        tag.parentNode.insertBefore(link, tag);
-        link.appendChild(tag);
-    });
-};
-
-// Create a link element from a text.
-function jiraLinkFromText(document, text) {
+// Creates a link element from a text.
+function _jiraLinkFromText(document, text) {
     let link = document.createElement("a");
     link.href = JIRALINK + text.toUpperCase();
     return link;
 };
 
+
+function transformTicketTags(ticketTags) {
+    // Choose only tags where text follows the pattern.
+    let jiraTags = ticketTags.filter(tag => _checkTagValidity);
+    if (jiraTags.length) {
+        return;
+    }
+    console.debug("Ticket tags to proces:", jiraTags.length);
+    // Wrap the tag with a link element.
+    jiraTags.forEach(function (tag) {
+        let link = _jiraLinkFromText(tag.innerText);
+        tag.parentNode.insertBefore(link, tag);
+        link.appendChild(tag);
+    });
+};
+
 exports.CardView = CardView;
 exports.TableView = TableView;
 exports.TicketView = TicketView;
-
-exports.transformTicketTags = transformTicketTags;
--- test/tags.js
+++ test/tags.js
@@ -1,23 +1,52 @@
-const jsdom = require("jsdom");
-const { JSDOM } = jsdom;
-const should = require('chai').should();
-const lib = require('../src/fresh-lib');
+const jsdom = require("jsdom"),
+    { JSDOM } = jsdom,
+    expect = require('chai').expect,
+    lib = require('../src/fresh-lib');
 
-let document;
-let root;
-let screen;
+const tagsForTest = [
+    "TST-1",
+    "1234",
+    "TST-TST",
+    "---TST///",
+    "tag",
+    "&nbsp;",
+    "---",
+    "{{123}}"
+];
+
+let document, screen;
 
 describe('Tags', () => {
-    describe('ticket', () => {
-        beforeEach(() => JSDOM.fromFile("test/assets/ticket-structure-tags.html")
+    describe('in ticket', () => {
+        let options = { contentType: "text/html" };
+        beforeEach(() => JSDOM.fromFile("test/assets/ticket-structure-tags.html", options)
             .then(dom => {
                 document = dom.window.document;
-                root = document.getElementById('ticket-details');
-                screen = new lib.TicketView(root);
+                const tagTemplate = document.querySelector("li > span.tag-options");
+                if (!tagTemplate) {
+                    throw new Error("Invalid template structure: <span> tag was not found")
+                }
+
+                const list = tagTemplate.parentElement.parentElement;
+                tagsForTest.slice(1).forEach(tagValue => {
+
+                    list.appendChild(list.firstElementChild.cloneNode(true));
+                });
+
+                for (let index = 0; index < list.childElementCount; index++) {
+                    const tag = list.children[index].firstElementChild;
+                    tag.innerHTML = tagsForTest[index];
+                }
+                screen = new lib.TicketView(document.getElementById('ticket-details'));
             }));
+        it('should be fetched', (done) => {
+            var tags = screen.getTags();
+            expect(tags).to.have.lengthOf(tagsForTest.length);
+            done();
+        });
         it('should be converted', (done) => {
             var result = 1;
-            result.should.be.equal(1);
+            expect(result).to.be.equal(1);
             done();
         });
     })
